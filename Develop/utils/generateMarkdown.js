f35a9e3f302ff34f43eb3191524063aaed4581a5
Patrick Abernathy <pat.abs@gmail.com>
2021-08-15 15:28:33 -0500

    fixed markdown errors

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,15 +1,15 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license = "MIT"){
+  if (license === "MIT"){
     return "![MIT License](https://shields.io/badge/license-MIT-green)"
-  } else if (license = "GPL"){
+  } else if (license === "GPL"){
     return "![GPL License](https://shields.io/badge/license-GPL-blue)"
-  } else if (license = "BSD"){
+  } else if (license === "BSD"){
     return "![BSD License](https://shields.io/badge/license-BSD-green)"
-  } else if (license = "APACHE"){
+  } else if (license === "APACHE"){
     return "![APACHE License](https://shields.io/badge/license-Apache-blue)"
-  } else if (license = "NONE"){
+  } else if (license === "NONE"){
     return ""
   }
 }
@@ -17,15 +17,15 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license = "MIT"){
-    return "![MIT License](https://www.mit.edu/~amini/LICENSE.md)"
-  } else if (license = "GPL"){
-    return "![GPL License](https://www.gnu.org/licenses/gpl-3.0.en.html)"
-  } else if (license = "BSD"){
-    return "![BSD License](https://opensource.org/licenses/BSD-3-Clause)"
-  } else if (license = "APACHE"){
-    return "![APACHE License](https://www.apache.org/licenses/LICENSE-2.0)"
-  } else if (license = "NONE"){
+  if (license === "MIT"){
+    return "[ MIT License](https://www.mit.edu/~amini/LICENSE.md)"
+  } else if (license === "GPL"){
+    return "[ GPL License](https://www.gnu.org/licenses/gpl-3.0.en.html)"
+  } else if (license === "BSD"){
+    return "[ BSD License](https://opensource.org/licenses/BSD-3-Clause)"
+  } else if (license === "APACHE"){
+    return "[ APACHE License](https://www.apache.org/licenses/LICENSE-2.0)"
+  } else if (license === "NONE"){
     return ""
   }
 }
@@ -33,38 +33,57 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
- return `##License
- ` +
-  renderLicenseBadge(license) +
-  renderLicenseLink(license) 
-}
+  if (license === "NONE"){
+    return "";
+  }else{
+ return `## License   
+ `
++  
+renderLicenseBadge(license) +
+      renderLicenseLink(license);
+}}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
-    #${data.title}
-    ##Description
-    ${data.description}
-    ##Table of Contents
-    *Installation Instructions
-    *Usage
-    *License
-    *Contributing
-    *Tests
-    *Questions
-    ##Installation
-    ${data.instructions}
-    ##Usage
-    ${data.usage}
-    ` + 
+# ${data.title}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+* [Installation-Instructions](#Installation-Instructions)
+* [Usage](#Usage)
+* [License](#License)
+* [Contributing](#Contributing)
+* [Tests](#Tests)
+* [Questions](#Questions)
+  
+## Installation-Instructions
+
+${data.instructions}
+
+## Usage
+
+${data.usage}
+
+` + 
     renderLicenseSection(data.license) +
-    `
-    ##Contributing
-    ${data.guidelines}
-    ##Tests
-    ${data.testing}
-    ##Questions
-    ${data.email} https://github.com/${data.username}
+`   
+## Contributing  
+
+${data.guidelines}  
+
+## Tests  
+
+${data.testing}  
+
+## Questions  
+
+${data.email}  
+https://github.com/${data.username}  
 
 `;
 }
