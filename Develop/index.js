ab6b86b2576aa619aa4851c82a94491064ce65a7
Patrick Abernathy <pat.abs@gmail.com>
2021-08-15 11:42:50 -0500

    working file

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,27 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{type: 'input',name: 'name',message: 'What is your name?'},
+{type: 'input',name: 'title',message: 'What do you do?'},
+{type: 'input',name: 'hobby',message: 'Spare time?'}
+
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+      inquirer.prompt(questions).then(answers => { const pageHTML = generateMarkdown(answers)
+      fs.writeFile('Dist/index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+       console.log('Page created! Check out index.html in this directory to see it!');
+     });});
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -13,7 +13,21 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  <!DOCTYPE html>
+  <html lang="en">
 
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Read Me</title>
+  </head>
+
+  <body>
+    <h1>${data.title}</h1>
+    
+  </body>
+  </html>
 `;
 }
 
