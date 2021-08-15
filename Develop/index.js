81668243f0cf0c5e0d71bf24d9771b96c9f42830
Patrick Abernathy <pat.abs@gmail.com>
2021-08-15 14:34:39 -0500

    converted html to markdown

--- Develop/index.js
+++ Develop/index.js
@@ -9,7 +9,7 @@ const questions = [{type: 'input',name: 'title',message: 'What is your project t
 {type: 'input',name: 'usage',message: 'What usage information would you like to add?'},
 {type: 'input',name: 'guidelines',message: 'What contribution guidelines do you have?'},
 {type: 'input',name: 'testing',message: 'Do you have testing instructions?'},
-{type: 'input',name: 'license',message: 'Please choose a license for your application.'},
+{type: 'list',name: 'license',choices:["MIT","GPL","BSD","APACHE","NONE"],message: 'Please choose a license for your application.'},
 {type: 'input',name: 'username',message: 'What is your github username?'},
 {type: 'input',name: 'email',message: 'What is your email address?'}
 ];
@@ -19,14 +19,14 @@ function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, err => {
         if (err) throw new Error(err);
   
-         console.log('Page created! Check out index.html in this directory to see it!');
+         console.log('Page created! Check out ReadMe.MD in this directory to see it!');
        });
 }
 
 // TODO: Create a function to initialize app
 function init() {
       inquirer.prompt(questions).then(answers => { const pageHTML = generateMarkdown(answers)
-        writeToFile('Dist/index.html', pageHTML);
+        writeToFile('Dist/ReadMe.MD', pageHTML);
 });
 }
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,53 +1,70 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license = "MIT"){
+    return "![MIT License](https://shields.io/badge/license-MIT-green)"
+  } else if (license = "GPL"){
+    return "![GPL License](https://shields.io/badge/license-GPL-blue)"
+  } else if (license = "BSD"){
+    return "![BSD License](https://shields.io/badge/license-BSD-green)"
+  } else if (license = "APACHE"){
+    return "![APACHE License](https://shields.io/badge/license-Apache-blue)"
+  } else if (license = "NONE"){
+    return ""
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license = "MIT"){
+    return "![MIT License](https://www.mit.edu/~amini/LICENSE.md)"
+  } else if (license = "GPL"){
+    return "![GPL License](https://www.gnu.org/licenses/gpl-3.0.en.html)"
+  } else if (license = "BSD"){
+    return "![BSD License](https://opensource.org/licenses/BSD-3-Clause)"
+  } else if (license = "APACHE"){
+    return "![APACHE License](https://www.apache.org/licenses/LICENSE-2.0)"
+  } else if (license = "NONE"){
+    return ""
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+ return `##License` +
+  renderLicenseBadge(license) +
+  renderLicenseLink(license) 
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
-  <!DOCTYPE html>
-  <html lang="en">
-
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>${data.title} Read Me</title>
-  </head>
-
-  <body>
-    <h1>${data.title}</h1>
-    <h2>Description</h2>
+    #${data.title}
+    ##Description
     ${data.description}
-    <h2>Table of Contents</h2>
-    <a href="#install">Installation Instructions</a>
-    <a href="#usage">Usage</a>
-    <a href="#license">License</a>
-    <a href="#contribute">Contributing</a>
-    <a href="#tests">Tests</a>
-    <a href="#questions">Questions</a>
-    <h2 id="install">Installation</h2>
+    ##Table of Contents
+    Installation Instructions (#Installation)
+    Usage
+    License
+    Contributing
+    Tests
+    Questions
+    ##Installation
     ${data.instructions}
-    <h2 id="usage">Usage</h2>
+    ##Usage
     ${data.usage}
-    <h2 id="license">License</h2>
-    ${data.license}
-    <h2 id="contribute">Contributing</h2>
+    ` + 
+    renderLicenseSection(data.license) +
+    `
+    ##Contributing
     ${data.guidelines}
-    <h2 id="tests">Tests</h2>
+    ##Tests
     ${data.testing}
-    <h2 id="questions">Questions</h2>
+    ##Questions
     ${data.email} https://github.com/${data.username}
-  </body>
-  </html>
+
 `;
 }
 
